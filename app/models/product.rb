d2e6f431b282a2be4df6840f82b217f2d714ca38
Sidney Reynaldo <sidneyreynaldo@gmail.com>
2013-02-06 16:56:51 -0200

    Validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,15 @@
 class Product < ActiveRecord::Base
-  attr_accessible :description, :image_url, :price, :title
+  attr_accessible :title, :description, :image_url, :price
+
+  validates :title, :description, :image_url, :presence => true
+
+  validates :title, :uniqueness => true
+
+  validates :image_url, :format => {
+  	:with    => %r{\.(gif|jpg|png)$}i,
+  	:message => 'deve ser uma URL para uma imagem GIF, JPG ou PNG.'
+  }
+
+  validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+
 end
