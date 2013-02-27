963a2085867d93b14fdcfce402eef76c23db7701
Sidney Reynaldo <sidneyreynaldo@gmail.com>
2013-02-27 15:24:57 -0300

    Treinamento

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,5 +1,9 @@
 class LineItem < ActiveRecord::Base
-  attr_accessible :cart_id, :product_id
-  belong_to :product
-  belong_to :cart
+  attr_accessible :cart_id, :product_id, :quantity
+  belongs_to :product
+  belongs_to :cart
+
+  def total_price
+    product.price * quantity
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -20,9 +20,8 @@ class Product < ActiveRecord::Base
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
 
   private
-  
-    def ensure_not_referenced_by_any_line_item
 
+    def ensure_not_referenced_by_any_line_item
       if line_items.empty?
         return true
       else
